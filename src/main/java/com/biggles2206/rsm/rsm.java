8ef5f2e5b46fe1943603029a4e66ca3d89e908c5
Thomas Saunders <biggles2206@hotmail.com>
2016-07-08 15:08:38 +0100

    Refactor Imports

--- src/main/java/com/biggles2206/rsm/rsm.java
+++ src/main/java/com/biggles2206/rsm/rsm.java
@@ -11,17 +11,15 @@ import com.biggles2206.rsm.items.ModItems;
 import com.biggles2206.rsm.lib.Refence;
 import com.biggles2206.rsm.proxy.CommonProxy;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.network.NetworkMod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.Mod;
 
 
 @Mod(name = Refence.NAME, modid = Refence.MODID, version = Refence.VERSION)
-@NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class rsm {
 
 	@SidedProxy(clientSide = Refence.CLIENT_PROXY_LOC, serverSide = Refence.COMMON_PROXY_LOC)
