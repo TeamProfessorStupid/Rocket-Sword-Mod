adb868ff1cd131b62035ed841da9795e56c255b7
Thomas Saunders <biggles2206@gmail.com>
2016-09-16 09:07:34 +0100

    Remove classes that are not needed

--- src/main/java/com/biggles2206/rsm/lib/BlockStrings.java
+++
@@ -1,9 +0,0 @@
-package com.biggles2206.rsm.lib;
-
-/**
- * @author biggles2206
- * @license Minecraft mod licence 1.0
- */
-public class BlockStrings {
-    public static final String BOUNDER_NAME = "Bounder";
-}
--- src/main/java/com/biggles2206/rsm/lib/MiscStrings.java
+++
@@ -1,9 +0,0 @@
-package com.biggles2206.rsm.lib;
-
-
-public class MiscStrings {
-
-	//Location for Configs
-	public static final String CONFIGLOCATIONDEFAULT = "RSM";
-	public static String CONFIGLOCATION = "RSM";
-}
