9148f380120ab47afbea797effda1ef45512c386
Thomas Saunders <biggles2206@gmail.com>
2016-09-16 15:34:53 +0100

    Refactor class to make more sense

--- src/main/java/com/biggles2206/rsm/RocketSwordMod.java
+++ src/main/java/com/biggles2206/rsm/RocketSwordMod.java
@@ -6,6 +6,7 @@ import com.biggles2206.rsm.util.config;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -13,25 +14,26 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 
 @Mod(name = config.NAME, modid = config.MODID, version = config.VERSION)
-public class rsm {
+public class RocketSwordMod {
 
+	@Instance
+	public static RocketSwordMod instance;
+	
 	@SidedProxy(clientSide = config.CLIENT_PROXY_LOC, serverSide = config.COMMON_PROXY_LOC)
 	public static CommonProxy proxy;
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event){
-		
+		ModItems.addItems();
+		ModItems.addNames();
+		ModItems.addItemCraftingRecipes();
+
 	}
 	
 	@EventHandler
 	public void Init(FMLInitializationEvent event){
-
-		ModItems.addItems();
-		ModItems.addNames();
-		ModItems.addItemCraftingRecipes();
-		
-		
-        }	
+		proxy.init();
+    }	
 	
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event){
