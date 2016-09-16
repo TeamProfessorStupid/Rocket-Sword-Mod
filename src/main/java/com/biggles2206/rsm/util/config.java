18b98f5e508d3265db1b7719f15db15cc86bdd46
Thomas Saunders <biggles2206@gmail.com>
2016-09-16 15:35:07 +0100

    Update modid and common proxy location

--- src/main/java/com/biggles2206/rsm/util/config.java
+++ src/main/java/com/biggles2206/rsm/util/config.java
@@ -5,10 +5,10 @@ package com.biggles2206.rsm.util;
 
 public class config {
 
-	public static final String MODID = "RocketSwordMod";
+	public static final String MODID = "rocketswordmod";
 	public static final String NAME = "Rocket Sword Mod";
 	public static final String VERSION = "0.0.1 Beta";
 	public static final String CLIENT_PROXY_LOC = "com.biggles2206.rsm.proxy.ClientProxy";
-	public static final String COMMON_PROXY_LOC = "com.biggles2206.rsm.proxy.CommonProxy";
+	public static final String COMMON_PROXY_LOC = "com.biggles2206.rsm.proxy.ServerProxy";
 
 }
